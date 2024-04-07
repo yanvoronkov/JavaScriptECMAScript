0020928ce860dd2e3e4eed63defbe17c146ae8a7
Yan Voronkov <sub.voronkov@gmail.com>
2024-04-07 20:40:10 +0300

    greate lesson3

--- lesson3/script.js
+++ lesson3/script.js
@@ -44,3 +44,106 @@ const employee = new Employee("John Smith");
 employee.displayInfo();
 const manager = new Manager("Jane Doe", "Sales");
 manager.displayInfo();
+
+
+// Задание 2: "Управление списком заказов"
+
+// Реализуйте класс Order(заказ), который имеет следующие свойства и методы:
+
+// Свойство orderNumber(номер заказа) - число, уникальный номер заказа.
+// Свойство products(продукты) - массив, содержащий список продуктов в заказе.
+// Метод addProduct(product) - принимает объект product и добавляет его в список продуктов заказа.
+// Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+
+// // Пример использования класса
+// class Product {
+// 	constructor(name, price) {
+// 		this.name = name;
+// 		this.price = price;
+// 	}
+// }
+
+// const order = new Order(12345);
+
+// const product1 = new Product(""Phone"", 500);
+// order.addProduct(product1);
+
+// const product2 = new Product(""Headphones"", 100);
+// order.addProduct(product2);
+
+// console.log(order.getTotalPrice()); // Вывод: 600
+
+class Order {
+	constructor(orderNumber) {
+		this.orderNumber = orderNumber;
+		this.products = [];
+	}
+	addProduct(product) {
+		this.products.push(product);
+	}
+	getTotalPrice() {
+		return this.products.reduce((total, product) => total + product.price, 0);
+	}
+}
+
+class Product {
+	constructor(name, price) {
+		this.name = name;
+		this.price = price;
+	}
+}
+
+const order = new Order(12345);
+
+const product1 = new Product("Phone", 500);
+order.addProduct(product1);
+
+const product2 = new Product("Headphones", 100);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice());
+
+// Задание 3: Управление животными в зоопарке
+// Вы разрабатываете систему управления животными в зоопарке.Вам нужно создать класс ZooAnimal, 
+// который будет иметь следующие характеристики:
+
+// Приватные поля:
+// * #name: имя животного(строка).
+// * #age: возраст животного(число).
+// * #gender: пол животного(строка).
+// Публичные методы:
+// * constructor(name, age, gender): конструктор класса, который инициализирует поля #name, #age и #gender.
+// * changeName(newName): изменяет имя животного на новое.
+// * changeAge(newAge): изменяет возраст животного на новый.
+
+// Статическое поле:
+// * MAX_AGE: максимально допустимый возраст для всех создаваемых объектов(число).
+
+// Ваша задача: реализовать класс ZooAnimal с указанными характеристиками.Убедитесь, что вы используете
+// приватные поля и статическое поле в соответствии с описанием.
+
+
+class ZooAnimal {
+	#name;
+	#age;
+	#gender;
+
+	static MAX_AGE = 15;
+	constructor(name, age, gender) {
+		this.#name = name;
+		this.#age = age;
+		if (age > ZooAnimal.MAX_AGE) {
+			throw new Error("Age is too big");
+		}
+		this.#gender = gender;
+	}
+	changeName(newName) {
+		this.#name = newName;
+	}
+	changeAge(newAge) {
+		this.#age = newAge;
+		if (newAge > ZooAnimal.MAX_AGE) {
+			throw new Error("Age is too big");
+		}
+	}
+}
